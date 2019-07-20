3102b68e9c3541ce38ce26e4b224799c1f0bbf14
Rene Ricardo <renerp2016@gmail.com>
2019-07-20 15:20:57 -0400

    adding stars. Adding night veil. styling some components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-import 'jquery';
-import 'popper.js';
-import "bootstrap";
-import "./js/app"
 import './sass/main.scss';
+import './js/app'
--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,21 @@ import * as $ from 'jquery'
 $(function () {
     console.log('Thanks for using html-shell!')
 });
+$(function () {
+    const starsWall = $('.stars-wall');
+    let top, left, t2;
+    const starsAmount = 80;
+    for (let i = 0; i <= starsAmount; i++) {
+        left = Math.ceil(Math.random() * 100);
+        top = Math.ceil(Math.random() * 100);
+        t2 = Math.ceil(Math.random() * 7) + 1;
+        const transform = `scale(0.${t2})`;
+        starsWall.append(`<span class="star" style="top:${top}%;left:${left}%;
+            -moz-transform:${transform};
+            -o-transform:${transform};
+            -ms-transform:${transform};
+            -webkit-transform:${transform};
+            transform:${transform};
+             "></span>`)
+    }
+});
